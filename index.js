6aee5aade48e8e526de96ec007a1f285ffa02d50
skyz03 <aakibdiscoo@gmail.com>
2023-11-10 09:52:18 +0545

    fix: code clean up

--- index.js
+++ index.js
@@ -1,51 +1,37 @@
-// Define a map of regions to their corresponding container IDs
-const regionMap = {
-  All: "https://skyz03.github.io/Coutry-Data-API/data.json",
-  Africa: "africa-region-data",
-  Americas: "americas-region-data",
-  Asia: "asia-region-data",
-  Europe: "europe-region-data",
-  Oceania: "oceania-region-data",
-};
+// Function to fetch and display the data
+async function fetchData() {
+  try {
+    const response = await fetch(
+      "https://skyz03.github.io/Coutry-Data-API/data.json"
+    );
+    const data = await response.json();
+    const filteredCountries = data.filter(
+      (country) => country.region === "Asia"
+    );
 
-// Function to filter and display data by region
-function displayDataByRegion(selectedRegion) {
-  const dataContainer = document.getElementById("data-container");
-  // dataContainer.innerHTML = ""; // Clear the previous data
+    if (Array.isArray(filteredCountries)) {
+      const dataContainer = document.getElementById("data-container");
 
-  // Get the region container ID
-  const regionContainerId = regionMap[selectedRegion];
+      filteredCountries.forEach((country) => {
+        const countryDiv = document.createElement("div");
+        countryDiv.innerHTML = `<h2>${country.name}</h2>
+                            <p>Population: ${country.population}</p>
+                            <p>Capital: ${country.capital}</p>
+                            <p>Region: ${country.region}</p>
+                            <p>Subregion: ${country.subregion}</p>
+                            <p>Alpha2 Code: ${country.alpha2Code}</p>
+                            <p>Alpha3 Code: ${country.alpha3Code}</p>
+                            <img src="${country.flag}" alt="${country.name} Flag">`;
 
-  // Filter and display data in the selected region container
-  data.forEach((country) => {
-    if (selectedRegion === "All" || country.region === selectedRegion) {
-      const countryDiv = document.createElement("div");
-      countryDiv.innerHTML = `<h2>${country.name}</h2>
-                <p>Population: ${country.population}</p>
-                <p>Capital: ${country.capital}</p>
-                <p>Region: ${country.region}</p>
-                <p>Subregion: ${country.subregion}</p>
-                <p>Alpha2 Code: ${country.alpha2Code}</p>
-                <p>Alpha3 Code: ${country.alpha3Code}</p>
-                <img src="${country.flag}" alt="${country.name} Flag">`;
-
-      const regionDataContainer = document.getElementById(regionContainerId);
-      regionDataContainer.appendChild(countryDiv);
+        dataContainer.appendChild(countryDiv);
+      });
+    } else {
+      console.error("Data is not in the expected format.");
     }
-  });
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
 }
 
-// Add event listeners to the region tabs to switch between regions
-const regionTabs = document.querySelectorAll("#region-tabs button");
-regionTabs.forEach((tab) => {
-  tab.addEventListener("click", () => {
-    // Remove the "active" class from all tabs
-    regionTabs.forEach((t) => t.classList.remove("active"));
-    // Add the "active" class to the clicked tab
-    tab.classList.add("active");
-    // Get the selected region from the tab's text
-    const selectedRegion = tab.textContent;
-    // Display data for the selected region
-    displayDataByRegion(selectedRegion);
-  });
-});
+// Call the fetchData function when the page loads
+fetchData();
