708c6330830931f3b12d8ef1a5ad8a65f7415779
skyz03 <aakibdiscoo@gmail.com>
2023-11-11 10:26:10 +0545

    Converted to react

--- countries-api/src/App.js
+++ countries-api/src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./index.css";
 
-function App() {
+const App = () => {
+  const [selectedRegion, setSelectedRegion] = useState("Asia");
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetchData(selectedRegion);
+  }, [selectedRegion]);
+
+  const fetchData = async (region) => {
+    try {
+      const response = await fetch(
+        "https://skyz03.github.io/Coutry-Data-API/data.json"
+      );
+      const data = await response.json();
+
+      const filteredCountries = data.filter(
+        (country) => country.region === region
+      );
+
+      if (Array.isArray(filteredCountries)) {
+        setCountries(filteredCountries);
+      } else {
+        console.error("Data is not in the expected format.");
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto p-4">
+      <h1 className="text-4xl text-red-400 font-bold mb-8">Country Data</h1>
+
+      {/* Dropdown menu for selecting regions */}
+      <label htmlFor="regionSelect" className="text-lg font-semibold mb-4">
+        Select a region:
+      </label>
+      <select
+        id="regionSelect"
+        onChange={(e) => setSelectedRegion(e.target.value)}
+        value={selectedRegion}
+        className="p-2 border rounded"
+      >
+        <option value="Asia">Asia</option>
+        <option value="Europe">Europe</option>
+        <option value="Africa">Africa</option>
+        <option value="Americas">Americas</option>
+        <option value="Oceania">Oceania</option>
+        {/* Add more options for other regions as needed */}
+      </select>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mt-8">
+        {/* Data will be displayed here */}
+        {countries.map((country) => (
+          <div
+            key={country.name}
+            className="bg-gray-200 border border-gray-300 p-4 rounded-md text-center transition-transform hover:scale-105"
+          >
+            <img
+              src={country.flag}
+              alt={`${country.name} Flag`}
+              className="mb-4 mx-auto"
+            />
+            <h2 className="text-lg font-semibold">{country.name}</h2>
+            <p>Population: {country.population}</p>
+            <p>Capital: {country.capital}</p>
+            <p>Region: {country.region}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
